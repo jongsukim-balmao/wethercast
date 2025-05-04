1154d2a2bb22ecb736a9e4a1144538423d5c4837
jongsukim-balmao <balmao8892@gmail.com>
2025-05-04 22:32:20 +0900

    Initial commit

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 var express = require("express");
 var app = express();
 // EXPRESS : 서버를 만들기위해 DOWN 받은 PACKAGE에서 EXPRESS를 호출함 //
 // /weather : 받은 정보를 저장하기 위한  SERVER에서 /WEATHER라는 WEB SITE로 경로로 들어갈때 마다  OPEN API가 호출 된다 //
 app.get("/weather", function (req: any, res: any) {
-  const { serviceKey, numOfRows, pageNo, base_date, base_time, nx, ny } =
+  const { numOfRows, pageNo, base_date, base_time, nx, ny } =
     req.query;
+  const serviceKey = process.env.SERVICE_KEY;
  // web site에서 들어온 REQUIRE요청을 처리한다. GET 방식으로 REQUIRE된 QUERY를 받은 정보((? 다음의  Query key =value의 내용을 저장하기 위한 변수 선언 //
   var api_url =
     "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?";
@@ -24,11 +27,10 @@ app.get("/weather", function (req: any, res: any) {
     }
   });
 });
-
 //http://127.0.0.1:3000/weather : WEB SERVER를 실행하기 위한 PORT를 설정, 진입시 APP을 //
 //호출하게 되는 PAGE SITE를 표시함 //
 app.listen(3000, function () {
   console.log(
-    "http://127.0.0.1:3000/weather?serviceKey=내인증키&numOfRows=10&pageNo=1&base_date=20241028&base_time=0600&nx=61&ny=125 app listening on port 3000!"
+    "http://127.0.0.1:3000/weather?serviceKey=serviceKey&numOfRows=10&pageNo=1&base_date=20241028&base_time=0600&nx=61&ny=125 app listening on port 3000!"
   );
 });
